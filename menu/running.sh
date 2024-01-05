88438d5e0ad4cbb1cabe3e6e58c08b9993e8896d
aaiki9 <155691347+aaiki9@users.noreply.github.com>
2024-01-05 17:22:01 +0700

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -287,6 +287,5 @@ echo -e "❇️ Websocket TLS           :$swstls"
 echo -e "❇️ Websocket None TLS      :$swstls"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo ""
-read -n 1 -s -r -p "Press any key to ```back``` on menu"
+read -n 1 -s -r -p "Press any key to back on menu"
 menu
-esac

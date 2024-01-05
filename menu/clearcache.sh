ae56dcdcafdcb37cb846b048b68cc7ba10639ecb
aaiki9 <155691347+aaiki9@users.noreply.github.com>
2024-01-05 13:54:35 +0700

    Add files via upload

---
+++ menu/clearcache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$(curl -sS https://raw.githubusercontent.com/aaiki9/autosc/main/permission/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+exit 0
+fi
+clear
+echo ""
+echo ""
+echo -e "[ \033[32mInfo\033[0m ] Clear RAM Cache"
+echo 1 > /proc/sys/vm/drop_caches
+sleep 3
+echo -e "[ \033[32mok\033[0m ] Cache cleared"
+echo ""
+echo "Back to menu in 2 sec "
+sleep 2
+menu

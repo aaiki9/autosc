acca49d621358f96a3585b33a02063f9df4140d1
aaiki9 <155691347+aaiki9@users.noreply.github.com>
2024-01-05 10:39:18 +0700

    Update renew.sh

--- ssh/renew.sh
+++ ssh/renew.sh
@@ -5,7 +5,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 
 PERMISSION () {
     MYIP=$(wget -qO ipinfo.io/io)
-    IZIN=$(wget -qO https://raw.githubusercontent.com/aaiki9/autosc/main/permission/ip | awk '{print $3}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/aaiki9/autosc/main/permission/ip | awk '{print $3}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
    # Bloman
     else
